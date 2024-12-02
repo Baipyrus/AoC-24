2f5e809c54bbb7a7133e4a32d594c8948fc3c3ed
Baipyrus <simon.waltemathe@gmail.com>
2024-12-02 16:21:48 +0100

    implement solution for challenge day01 part1

--- internal/day01/part1/main.go
+++ internal/day01/part1/main.go
@@ -2,6 +2,9 @@ package day01_part1
 
 import (
 	"fmt"
+	"regexp"
+	"slices"
+	"strconv"
 
 	"github.com/Baipyrus/AoC-24/internal/registry"
 )
@@ -14,4 +17,37 @@ func init() {
 
 func Main(input string) {
 	fmt.Printf("Executing: %s\n", name)
+
+	var left, right []uint64
+
+	lines := regexp.MustCompile(`\r*\n`).Split(input, -1)
+	for _, line := range lines {
+		values := regexp.MustCompile(`\s+`).Split(line, -1)
+		if len(values) != 2 {
+			continue
+		}
+
+		l, _ := strconv.ParseUint(values[0], 10, 64)
+		r, _ := strconv.ParseUint(values[1], 10, 64)
+
+		left = append(left, l)
+		right = append(right, r)
+	}
+
+	slices.Sort(left)
+	slices.Sort(right)
+
+	var sum uint64
+	for idx := range left {
+		l := left[idx]
+		r := right[idx]
+
+		if l < r {
+			sum += r - l
+		} else {
+			sum += l - r
+		}
+	}
+
+	fmt.Printf("Total distance between lists: %d\n", sum)
 }
