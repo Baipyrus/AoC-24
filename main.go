53dc67c33f4d803e3607e7258226baa52df0f952
Baipyrus <simon.waltemathe@gmail.com>
2024-12-02 14:48:34 +0100

    register challenges for execution from main

--- main.go
+++ main.go
@@ -3,40 +3,22 @@ package main
 import (
 	"log"
 
+	registry "github.com/Baipyrus/AoC-24/internal"
 	"github.com/ktr0731/go-fuzzyfinder"
 )
 
-type Track struct {
-    Name      string
-    AlbumName string
-    Artist    string
-}
-
-var tracks = []Track{
-    {"foo", "album1", "artist1"},
-    {"bar", "album1", "artist1"},
-    {"foo", "album2", "artist1"},
-    {"baz", "album2", "artist2"},
-    {"baz", "album3", "artist2"},
-}
-
 func main() {
-    idx, err := fuzzyfinder.FindMulti(
-        tracks,
-        func(i int) string {
-            return tracks[i].Name
-        },
-        fuzzyfinder.WithPreviewWindow(func(i, w, h int) string {
-            if i == -1 {
-                return ""
-            }
-            return fmt.Sprintf("Track: %s (%s)\nAlbum: %s",
-                tracks[i].Name,
-                tracks[i].Artist,
-                tracks[i].AlbumName)
-        }))
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Printf("selected: %v\n", idx)
+	challenges := registry.Get()
+
+	idx, err := fuzzyfinder.Find(
+		challenges,
+		func(i int) string {
+			return challenges[i].Name
+		},
+		fuzzyfinder.WithPromptString("Select Challenge: "))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	challenges[idx].Exec()
 }
