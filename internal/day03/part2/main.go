85c1e34a08494ecd10f3913b38d3a8ad16d94c5b
waltem01 <simon.waltemathe@ptb.de>
2024-12-03 13:45:50 +0100

    implement solution for challenge day03 part2

--- internal/day03/part2/main.go
+++ internal/day03/part2/main.go
@@ -17,6 +17,31 @@ func init() {
 func Main(input string) {
 	fmt.Printf("Executing: %s\n", name)
 
-	var sum int64
+	r := regexp.MustCompile(`mul\((-?\d{1,3}),(-?\d{1,3})\)|don't\(\)|do\(\)`)
+	matches := r.FindAllStringSubmatch(input, -1)
+
+	var (
+		sum int64
+		use = true
+	)
+
+	for _, match := range matches {
+		switch match[0] {
+		case "don't()":
+			use = false
+		case "do()":
+			use = true
+		}
+
+		if !use {
+			continue
+		}
+
+		l, _ := strconv.ParseInt(match[1], 10, 64)
+		r, _ := strconv.ParseInt(match[2], 10, 64)
+
+		sum += l * r
+	}
+
 	fmt.Printf("Sum of multiplications: %d\n", sum)
 }
