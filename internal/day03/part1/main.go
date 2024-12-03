2411b0098377e3b57ee5fe3027c9b68e2e9a4277
waltem01 <simon.waltemathe@ptb.de>
2024-12-03 12:05:44 +0100

    implement solution for challenge day03 part1

--- internal/day03/part1/main.go
+++ internal/day03/part1/main.go
@@ -2,6 +2,8 @@ package day03_part1
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 
 	"github.com/Baipyrus/AoC-24/internal/registry"
 )
@@ -14,4 +16,17 @@ func init() {
 
 func Main(input string) {
 	fmt.Printf("Executing: %s\n", name)
+
+	r := regexp.MustCompile(`mul\((-?\d{1,3}),(-?\d{1,3})\)`)
+	matches := r.FindAllStringSubmatch(input, -1)
+
+	var sum int64
+	for _, match := range matches {
+		l, _ := strconv.ParseInt(match[1], 10, 64)
+		r, _ := strconv.ParseInt(match[2], 10, 64)
+
+		sum += l * r
+	}
+
+	fmt.Printf("Sum of multiplications: %d\n", sum)
 }
