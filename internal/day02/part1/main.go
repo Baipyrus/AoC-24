f0904e91438b27a32b0a501ff75d03098e7e8d2b
Baipyrus <simon.waltemathe@gmail.com>
2024-12-04 10:15:27 +0100

    implement solution for challenge day02 part1

--- internal/day02/part1/main.go
+++ internal/day02/part1/main.go
@@ -2,6 +2,8 @@ package day02_part1
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/Baipyrus/AoC-24/internal/registry"
 )
@@ -14,4 +16,46 @@ func init() {
 
 func Main(input string) {
 	fmt.Printf("Executing: %s\n", name)
+
+	var count uint64
+	lines := strings.Split(input, "\n")
+
+	for _, line := range lines {
+		reports := strings.Split(line, " ")
+		if len(reports) == 0 || reports[0] == "" {
+			continue
+		}
+
+		var (
+			direction int64
+			previous  uint64
+			safe      bool = true
+		)
+
+		for idx, report := range reports {
+			v, _ := strconv.ParseUint(report, 10, 64)
+			if idx == 0 {
+				previous = v
+				continue
+			}
+
+			diff := int64(v - previous)
+			up := diff > 0 && direction < 0
+			down := diff < 0 && direction > 0
+			tole := diff > 3 || diff < -3 || diff == 0
+			if up || down || tole {
+				safe = false
+				break
+			}
+
+			direction = diff
+			previous = v
+		}
+
+		if safe {
+			count++
+		}
+	}
+
+	fmt.Printf("Amount of safe reports: %d\n", count)
 }
