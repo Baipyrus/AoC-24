a9ec636ac39ad39d8722a4f610be3e5e3aad6e8e
Baipyrus <simon.waltemathe@gmail.com>
2024-12-07 17:03:33 +0100

    implement solution for challenge day07 part2

--- internal/day07/part2/main.go
+++ internal/day07/part2/main.go
@@ -2,6 +2,8 @@ package day07_part2
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/Baipyrus/AoC-24/internal/registry"
 )
@@ -14,4 +16,97 @@ func init() {
 
 func Main(input string) {
 	fmt.Printf("Executing: %s\n", name)
+
+	lines := strings.Split(input, "\n")
+	var equations []Equation
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		members := strings.Split(line, " ")
+
+		first := members[0]
+		trim := first[:len(first)-1]
+		result, _ := strconv.ParseInt(trim, 10, 64)
+
+		var values []int64
+		for i := 1; i < len(members); i++ {
+			v, _ := strconv.ParseInt(members[i], 10, 64)
+			values = append(values, v)
+		}
+
+		equations = append(equations, Equation{
+			Result: result,
+			Values: values})
+	}
+
+	var sum int64
+	for _, eq := range equations {
+		if eq.Validate() {
+			sum += eq.Result
+		}
+	}
+
+	fmt.Printf("Sum of valid calibration results: %d\n", sum)
+}
+
+type Equation struct {
+	Result int64
+	Values []int64
+}
+
+func (e *Equation) Validate() bool {
+	var operators int64 = 3 // Add + Mult + Concat
+	maximum := intPow(operators, int64(len(e.Values)-1))
+
+	// State is a sequence of operators ("+, +, +" = 0 or "+, +, *" = 1 or ...)
+	var state int64
+	for state = range maximum {
+		result := e.Values[0]
+
+		// Go through every value
+		for idx := int64(1); idx < int64(len(e.Values)); idx++ {
+			curr := e.Values[idx]
+			// Extract operator for current position from state
+			op := (state / intPow(operators, idx-1)) % operators
+
+			switch op {
+			case 0:
+				// Add to result
+				result += curr
+			case 1:
+				// Multiply result
+				result *= curr
+			case 2:
+				// Concat result
+				str := fmt.Sprintf("%d%d", result, curr)
+				result, _ = strconv.ParseInt(str, 10, 64)
+			}
+		}
+
+		// If equation satisfied, return valid
+		if result == e.Result {
+			return true
+		}
+	}
+
+	return false
+}
+
+func intPow(base, expo int64) int64 {
+	if expo == 0 {
+		return 1
+	} else if expo == 1 {
+		return base
+	}
+
+	result := base
+
+	var iter int64
+	for iter = 2; iter <= expo; iter++ {
+		result *= base
+	}
+	return result
 }
