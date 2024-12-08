c56b0fab2a23c67d1af860a2a323651ef0696543
Baipyrus <simon.waltemathe@gmail.com>
2024-12-08 19:35:57 +0100

    initialize challenge day08 part2

--- challenges.go
+++ challenges.go
@@ -16,4 +16,5 @@ import (
 	_ "github.com/Baipyrus/AoC-24/internal/day07/part1"
 	_ "github.com/Baipyrus/AoC-24/internal/day07/part2"
 	_ "github.com/Baipyrus/AoC-24/internal/day08/part1"
+	_ "github.com/Baipyrus/AoC-24/internal/day08/part2"
 )
