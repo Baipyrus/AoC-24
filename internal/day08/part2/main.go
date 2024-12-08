7e050aa3348b7759d2cba3c670fb163095223aed
Baipyrus <simon.waltemathe@gmail.com>
2024-12-08 19:50:30 +0100

    implement solution for challenge day08 part2

--- internal/day08/part2/main.go
+++ internal/day08/part2/main.go
@@ -3,6 +3,7 @@ package day08_part2
 import (
 	"fmt"
 
+	"github.com/Baipyrus/AoC-24/internal/day08"
 	"github.com/Baipyrus/AoC-24/internal/registry"
 )
 
@@ -14,4 +15,39 @@ func init() {
 
 func Main(input string) {
 	fmt.Printf("Executing: %s\n", name)
+
+	grid := day08.ParseInput(input)
+	CalculateAntinodes(&grid)
+
+	fmt.Printf("Unique locations within bounds: %d\n", len(grid.Antinodes))
+}
+
+func CalculateAntinodes(grid *day08.Grid) {
+	groups := grid.GroupAntennas()
+
+	for _, positions := range groups {
+		for i, self := range positions {
+			for j, other := range positions {
+				if i == j {
+					continue
+				}
+
+				grid.TryAddAntinode(self)
+				diff := self.Sub(other)
+				previous := self
+
+				for {
+					next := diff.Add(previous)
+
+					bound := grid.OutOfBounds(next)
+					if bound {
+						break
+					}
+
+					grid.TryAddAntinode(next)
+					previous = next
+				}
+			}
+		}
+	}
 }
