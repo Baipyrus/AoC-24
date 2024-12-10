95a0d12d37ef10ba2cf0c9a8d22d5f80b48af984
Baipyrus <simon.waltemathe@gmail.com>
2024-12-10 11:43:18 +0100

    implement solution for challenge day09 part2

--- internal/day09/part2/main.go
+++ internal/day09/part2/main.go
@@ -16,6 +16,107 @@ func init() {
 func Main(input string) {
 	fmt.Printf("Executing: %s\n", name)
 
-	var checksum uint64
+	filesystem := parseInput(input)
+	compactBlocks(&filesystem)
+	checksum := calcChecksum(filesystem)
+
 	fmt.Printf("Checksum of compacted disk blocks: %d\n", checksum)
+}
+
+func calcChecksum(fs []File) (sum uint64) {
+	// Sum products of index and file id
+	var idx uint64 = 0
+
+	for _, file := range fs {
+		if file.Empty {
+			idx += file.Size
+			continue
+		}
+
+		for range file.Size {
+			sum += uint64(idx) * file.Id
+			idx++
+		}
+	}
+
+	return sum
+}
+
+func compactBlocks(fs *[]File) {
+	// Save slice for better readability
+	filesystem := *fs
+
+	for i := len(filesystem) - 1; i >= 0; i-- {
+		// Look for (non-empty) file blocks
+		current := filesystem[i]
+		if current.Empty {
+			continue
+		}
+
+		// Find left-most empty blocks
+		j := slices.IndexFunc(filesystem[:i], func(f File) bool {
+			return f.Empty && f.Size >= current.Size
+		})
+		if j == -1 {
+			continue
+		}
+
+		// Get difference in size before change
+		diff := filesystem[j].Size - current.Size
+
+		// Move file blocks into empty slots
+		filesystem[j].Id = current.Id
+		filesystem[j].Size = current.Size
+		filesystem[j].Empty = false
+
+		// Mark the old blocks as empty
+		filesystem[i].Empty = true
+
+		if diff > 0 {
+			sub := File{
+				Empty: true,
+				Size:  diff}
+			// Insert the new empty block after the moved block
+			filesystem = append(
+				filesystem[:j+1],
+				append(
+					[]File{sub},
+					filesystem[j+1:]...,
+				)...)
+		}
+	}
+
+	// Write the modified slice back to the pointer
+	*fs = filesystem
+}
+
+func parseInput(input string) (fs []File) {
+	var id uint64
+
+	for idx, char := range input {
+		// Invalid character
+		if char < '0' || char > '9' {
+			continue
+		}
+
+		amount := uint64(char - '0')
+		empty := idx%2 == 1
+
+		file := File{Empty: empty, Size: amount}
+
+		if !empty {
+			file.Id = id
+			id++
+		}
+
+		fs = append(fs, file)
+	}
+
+	return fs
+}
+
+type File struct {
+	Id    uint64
+	Size  uint64
+	Empty bool
 }
