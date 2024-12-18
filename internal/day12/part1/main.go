ac823fd163c57b10fda8cd83e89b58f9591c5d34
Baipyrus <simon.waltemathe@gmail.com>
2024-12-18 17:52:55 +0100

    implement solution for challenge day12 part1

--- internal/day12/part1/main.go
+++ internal/day12/part1/main.go
@@ -2,6 +2,8 @@ package day12_part1
 
 import (
 	"fmt"
+	"slices"
+	"strings"
 
 	"github.com/Baipyrus/AoC-24/internal/registry"
 )
@@ -14,4 +16,122 @@ func init() {
 
 func Main(input string) {
 	fmt.Printf("Executing: %s\n", name)
+
+	regions := parseInput(input)
+	var price uint
+
+	for _, r := range regions {
+		area := uint(len(r.Plots))
+		current := area * r.Perimeter()
+		price += current
+	}
+
+	fmt.Printf("Total price of fencing all regions: %d\n", price)
+}
+
+func parseInput(input string) (regions []*Region) {
+	lines := strings.Split(input, "\n")
+	counters := make(map[rune]uint)
+
+	for j, line := range lines {
+	loop:
+		for i, char := range line {
+			current := Component{X: i, Y: j}
+
+			for _, r := range regions {
+				// Plot not of the same type
+				if char != r.Char {
+					continue
+				}
+				if r.TryAdd(&current) {
+					continue loop
+				}
+			}
+
+			id := counters[char]
+			next := Region{
+				Id:    id,
+				Char:  char,
+				Plots: []*Component{&current}}
+			regions = append(regions, &next)
+
+			counters[char]++
+		}
+	}
+
+	return regions
+}
+
+type Component struct {
+	X int
+	Y int
+}
+
+type Region struct {
+	Id    uint
+	Char  rune
+	Plots []*Component
+}
+
+func (r *Region) Println() {
+	fmt.Printf("#%d: %c -", r.Id, r.Char)
+	for _, p := range r.Plots {
+		fmt.Printf(" %d %d,", p.X, p.Y)
+	}
+	fmt.Println()
+}
+
+func (r *Region) Perimeter() (sum uint) {
+	for _, a := range r.Plots {
+		sum += 4 - r.Neighbors(a)
+	}
+
+	return sum
+}
+
+func (r *Region) TryAdd(c *Component) bool {
+	// Plot already listed in Region
+	if r.Contains(*c) {
+		return false
+	}
+
+	neighboring := r.Neighbors(c) > 0
+
+	// Add if neighbor exists
+	if neighboring {
+		r.Plots = append(r.Plots, c)
+	}
+
+	return neighboring
+}
+
+func (r *Region) Neighbors(c *Component) (count uint) {
+	x, y := c.X, c.Y
+
+	for _, n := range r.Plots {
+		nx, ny := n.X, n.Y
+		dx, dy := nx-x, ny-y
+
+		//  0 = self
+		//  2 = diagonal
+		// >2 = invalid
+		sum := dx*dx + dy*dy
+		if sum == 1 {
+			count++
+		}
+	}
+
+	return count
+}
+
+func (r *Region) Contains(c Component) bool {
+	return hasPlot(r.Plots, c)
+}
+
+func hasPlot(s []*Component, p Component) bool {
+	return slices.ContainsFunc(
+		s,
+		func(c *Component) bool {
+			return *c == p
+		})
 }
